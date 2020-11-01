e2d6f23eb6e86f8f1ed86580f22907d7e021d22b
Paula Fleck <paulakfleck@gmail.com>
2020-11-01 18:27:29 -0300

    Adding logo and more

--- src/App.vue
+++ src/App.vue
@@ -2,48 +2,53 @@
   <div>
     <fixed-header>
       <header>
-        <ul>
-          <li>
-            Menu 1
-          </li>
-          <li>
-            Menu 2
-          </li>
-        </ul>
+        <div class="wrap-container">
+          <ul id="side-bar-menu">
+            <li>
+              Menu 1
+            </li>
+            <li>
+              Menu 2
+            </li>
+          </ul>
+          <a href="./" id="logo">The Secret Ingredient</a>
+        </div>
       </header>
     </fixed-header>
     <main>
-    <div id="welcome-message">
-    <h1>Olá, {{ user.name }}!</h1>
-    </div>
-      <search>
-        <form>
-          <input type="text" placeholder="Buscar ingrediente..." />
-          <button>
-            Search
-          </button>
-        </form>
-      </search>
-      <avoid-items>
-        <ul>
-          <li>
-            Item 1
-          </li>
-          <li>
-            Item 2
-          </li>
-        </ul>
-      </avoid-items>
-      <favorite-items>
-        <ul>
-          <li>
-            Item 1
-          </li>
-          <li>
-            Item 2
-          </li>
-        </ul>
-      </favorite-items>
+    <div class="wrap-container">
+      <div id="welcome-message">
+      <h1>Olá, {{ user.name }}!</h1>
+      </div>
+        <search>
+          <form>
+            <input type="text" placeholder="Buscar ingrediente..." />
+            <button>
+              Search
+            </button>
+          </form>
+        </search>
+        <avoid-items>
+          <ul>
+            <li>
+              Item 1
+            </li>
+            <li>
+              Item 2
+            </li>
+          </ul>
+        </avoid-items>
+        <favorite-items>
+          <ul>
+            <li>
+              Item 1
+            </li>
+            <li>
+              Item 2
+            </li>
+          </ul>
+        </favorite-items>
+      </div>
     </main>
   </div>
 </template>
