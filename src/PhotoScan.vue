c644451b9f0f8e996b08b94bc1c68bb6e7e5307a
Paula Fleck <paulakfleck@gmail.com>
2020-11-23 20:11:36 -0300

    Adding VueRouter

---
+++ src/PhotoScan.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    PhotoScan
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'photo-scan',
+
+  data () {
+    return {
+    }
+  }
+}
+</script>
--- src/main.js
+++ src/main.js
@@ -1,9 +1,22 @@
+
 import Vue from 'vue'
+import VueRouter from 'vue-router'
 import Homepage from './Homepage.vue'
+import PhotoScan from './PhotoScan.vue'
 
 import './assets/styles/homepage.scss';
 
-new Vue({
-  el: '#app',
-  render: h => h(Homepage)
+Vue.use(VueRouter)
+
+const routes = [
+  { path: '/', component: Homepage },
+  { path: '/photo-scan', component: PhotoScan }
+]
+
+const router = new VueRouter({
+  routes
 })
+
+const app = new Vue({
+  router
+}).$mount('#app')
