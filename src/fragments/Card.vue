aad47c6ea10757522c7d6e90107551aed36f6f4f
Paula Fleck <paulakfleck@gmail.com>
2021-03-27 23:15:34 -0300

    Card component

---
+++ src/fragments/Card.vue
@@ -0,0 +1,26 @@
+<template>
+  <article class="card" :class='{active: expandCard}'>
+    <h3 class="card-title">{{ingredient}} - <span>{{originalIngredient}}</span> </h3>
+    <button class="card-expand-collapse" @click="expandCard = !expandCard">{{expandCard ? 'Collapse' : 'Expand' }}</button>
+
+    <div class="card-description">
+      <p>{{intro}}</p>
+    </div>
+  </article>
+</template>
+
+<script>
+export default {
+  name: "card",
+
+  props: ['originalIngredient', 'ingredient', 'intro'],
+
+
+  data() {
+
+    return {
+      expandCard: false
+    }
+  },
+};
+</script>
